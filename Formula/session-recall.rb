ea99491c1309112973a741ee510862ff8fef08fc
Xiawei Yuan <yxwyoyoyo@gmail.com>
2026-08-14 12:35:13 +0100

    session-recall 0.3.0

--- Formula/session-recall.rb
+++ Formula/session-recall.rb
@@ -5,21 +5,21 @@ class SessionRecall < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.2.0/session-recall_0.2.0_Darwin_arm64.tar.gz"
-      sha256 "91e3eb84834361d44b8280e31999aca6e3d89f084fcb6f2edd58cf78dc736f40"
+      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.3.0/session-recall_0.3.0_Darwin_arm64.tar.gz"
+      sha256 "7631197342b62c81cdd1a54cd2f22a2a02a325dd5d8ff663bf22b0d3474f82f5"
     else
-      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.2.0/session-recall_0.2.0_Darwin_x86_64.tar.gz"
-      sha256 "64f31d57cfa24209a41359a882b07f3d4a3c9c0f3c9f9daed32330118ce7b1a3"
+      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.3.0/session-recall_0.3.0_Darwin_x86_64.tar.gz"
+      sha256 "97b9d5c66dd0044a41116354affa3b18e0dfff99cb6b8a2a848d6183275ed38d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.2.0/session-recall_0.2.0_Linux_arm64.tar.gz"
-      sha256 "e5e6f12ed3db6e2cc9498b3f6e71f61c4b49f0a942f2e181e029407159ea4553"
+      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.3.0/session-recall_0.3.0_Linux_arm64.tar.gz"
+      sha256 "7d1818fa10038ddebf8fd64803c42383b5a70c6f5404d5ac92e62f1c7b95e9ea"
     else
-      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.2.0/session-recall_0.2.0_Linux_x86_64.tar.gz"
-      sha256 "ea0d4392dd33bead9ddc41a936a0ddb17878f6f0fa4f04fb2e774d6c6385c4c5"
+      url "https://github.com/yxwyoyoyo/session-recall/releases/download/v0.3.0/session-recall_0.3.0_Linux_x86_64.tar.gz"
+      sha256 "3a4d98c8fa64f8ed97f01a2cb8cf8988e1111e17a0d482e8af02c97d0dd5baac"
     end
   end
 
